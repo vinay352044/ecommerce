156aa02de2fb2f9f40475f57775a678eb68ff4a2
Djkarmur <dipakkarmur45@gmail.com>
2024-03-12 13:36:25 +0530

    product card display

--- Client/src/components/pages/Home/Home.jsx
+++ Client/src/components/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import "./home.css"
+import ProductCard from "./FetchData"
 
 const Home = () => {
 	return (
 		<div className="home">
 			<h1 className="text-3xl">Home</h1>
+			<ProductCard/>
 		</div>
 	)
 }
--- Client/src/redux/actions/productActions.js
+++ Client/src/redux/actions/productActions.js
@@ -1,8 +1,30 @@
+import axios from "axios"
+import { setLoader } from "./appActions"
+import { API } from "../../utils/axios-instance"
+
 export const FETCH_PRODUCTS = 'FETCH_PRODUCTS'
 
 export const fetchProductsSuccess = (products) => {
   return{
     type: FETCH_PRODUCTS,
     payload: products
+  }
+}
+
+//////  fetching product data using redux-thunk
+export const fetchProductData = () =>{
+  return async (dispatch) =>{
+     try{
+      dispatch(setLoader(true))
+      const response = await API.get("/");
+      
+      console.log(response.data);
+      dispatch(fetchProductsSuccess(response.data))
+      dispatch(setLoader(false))
+
+     }catch(error){
+        console.log("Error Occured while fetching data:",error);
+     }
+    
   }
 }
