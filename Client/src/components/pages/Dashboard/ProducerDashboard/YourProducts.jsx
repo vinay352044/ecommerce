a49158754e5b87f070ed675f24f9dc572fea731c
Meet5555 <bhimanimeet55555@gmail.com>
2024-03-14 11:57:11 +0530

    showing filtered products to seller of their brand only

--- Client/src/components/pages/Dashboard/ProducerDashboard/YourProducts.jsx
+++ Client/src/components/pages/Dashboard/ProducerDashboard/YourProducts.jsx
@@ -1,67 +1,64 @@
-import React, {useEffect, useState} from 'react'
-import {DeleteProductbyId, getProducts} from '../../../../utils/axios-instance';
-import {useNavigate} from 'react-router-dom';
-import Table from '../../../common/Table';
+import React, { useEffect, useState } from "react"
+import { DeleteProductbyId, getProducts } from "../../../../utils/axios-instance"
+import { useNavigate } from "react-router-dom"
+import Table from "../../../common/Table"
+import { useSelector } from "react-redux"
 
 const YourProducts = () => {
-  const [products, setProducts] = useState([])
-  const navigate = useNavigate()
+	const [products, setProducts] = useState([])
+	const navigate = useNavigate()
+	const {
+		seller: { productsToSell: sellerProducts },
+	} = useSelector((state) => state.role)
 
-  const handleCreateProduct = () => {
-    navigate('/seller-create-products');
-  };
+	const handleProductUpdate = (productID) => {
+		navigate(`/seller-update-products/${productID}`)
+	}
 
-  const handleProductUpdate = (productID) => {
-    console.log(productID)
-    navigate(`/seller-update-products/${productID}`);
-  }
+	const handleProductDelete = async (productID) => {
+		const shouldDelete = window.confirm("Are you sure you want to delete this product?")
 
-  const handleProductDelete = async (productID) => {
-    console.log(productID);
+		if (!shouldDelete) {
+			return
+		}
 
-    const shouldDelete = window.confirm("Are you sure you want to delete this product?");
+		try {
+			const response = await DeleteProductbyId(productID)
+			if (response.success) {
+				setProducts((prevProducts) => prevProducts.filter((product) => product.id !== productID))
+			} else {
+				console.error("Failed to delete the Products Data", response.error)
+			}
+		} catch (error) {
+			console.error("Failed to delete the Products Data", error)
+		}
+	}
 
-    if (!shouldDelete) {
-      return;
-    }
+	const fetchData = async () => {
+		try {
+			const response = await getProducts()
+			if (response.success) {
+				const productsToShow = response.data.filter((product) => {
+					return sellerProducts.includes(product.id)
+				})
+				setProducts(productsToShow)
+			} else {
+				console.error("Failed to fetch the Products Data", response.error)
+			}
+		} catch (error) {
+			console.error("Error while Fetching products", error)
+		}
+	}
 
-    try {
-      const response = await DeleteProductbyId(productID);
-      if (response.success) {
-        console.log("Product Deleted Successfully!");
+	useEffect(() => {
+		fetchData()
+	}, [sellerProducts])
 
-        setProducts((prevProducts) => prevProducts.filter(product => product.id !== productID));
-      } else {
-        console.error('Failed to delete the Products Data', response.error);
-      }
-    } catch (error) {
-      console.error('Failed to delete the Products Data', error);
-    }
-  };
-
-  const fetchData = async () => {
-    try {
-      const response = await getProducts();
-      if (response.success) {
-        setProducts(response.data);
-        console.log(response)
-      } else {
-        console.error('Failed to fetch the Products Data', response.error);
-      }
-    } catch (error) {
-      console.error('Error while Fetching products', error);
-    }
-  };
-  
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  return (
-    <div>
-            <Table products={products} handleProductUpdate={handleProductUpdate} handleProductDelete={handleProductDelete} />
-    </div>
-  )
+	return (
+		<div>
+			<Table products={products} handleProductUpdate={handleProductUpdate} handleProductDelete={handleProductDelete} />
+		</div>
+	)
 }
 
 export default YourProducts
