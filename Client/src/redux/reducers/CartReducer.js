f3e26cf1f36e517131c9499c43031ef1b6f9eaf2
Djkarmur <dipakkarmur45@gmail.com>
2024-03-14 09:34:00 +0530

    quantity change functionality added in products

--- Client/src/redux/reducers/CartReducer.js
+++ Client/src/redux/reducers/CartReducer.js
@@ -1,25 +1,35 @@
-import { ADD_TO_CART, REMOVE_FROM_CART } from "../actions/productActions";
-
+ import { ADD_TO_CART, QUANTITY, REMOVE_FROM_CART } from "../actions/productActions";
 
 const initialState = {
     cartItems:[]
+  
 }
 
-export const CartReducer = (state = initialState,action) =>{
-    switch(action.type){
+export const CartReducer = (state = initialState, action) => {
+    switch(action.type) {
         case ADD_TO_CART:
-            const arrayState = state.cartItems;
-            return( {
-              cartItems: [...arrayState, action.payload]
-            })
-        
+            
+            const updatedCartItems = [...state.cartItems, action.payload];
+            const uniqueArray = [...new Set(updatedCartItems)]; 
+
+            return { ...state, cartItems: uniqueArray }; 
         case REMOVE_FROM_CART:
-            const arrayData  = state.cartItems;
-            const updatedArray = arrayData.filter((item)=> item.id !==action.payload)
-            return({
-                cartItems:updatedArray
-            })
+            
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(item => item.id !== action.payload)
+            };
+
+         case QUANTITY:
+            const index = state.cartItems.findIndex(item => item.id === action.payload.id);
+            if (index !== -1) {
+               
+                const updatedItems = [...state.cartItems];
+                updatedItems[index] = { ...updatedItems[index], quantity: action.payload.quantity };
+                return { ...state, cartItems: updatedItems };
+            }
+            return state; 
         default:
-           return state;
+            return state;
     }
-}
+};
