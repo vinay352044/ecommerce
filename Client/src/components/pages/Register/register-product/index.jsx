cbca164c28abd68c6195e81eca68495b1f3ea6d1
Vaideek Patel <84132883+vaideekpatel@users.noreply.github.com>
2024-03-14 22:19:53 +0530

    Styling-Bug Fixes

--- Client/src/components/pages/Register/register-product/index.jsx
+++ Client/src/components/pages/Register/register-product/index.jsx
@@ -68,104 +68,107 @@ const Index = () => {
 		}
 	}
 	return (
-		<Formik initialValues={InitialValues} onSubmit={handleSubmit}>
-			<div className="container mx-auto p-4">
-				<Form className="w-full max-w-lg">
-					<div className="flex flex-wrap -mx-3 mb-6">
-						<div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-							<label htmlFor="title" className={inputStyle}>
-								Title
-							</label>
-							<Field type="text" id="title" name="title" placeholder="Product Title" className={inputStyle1} />
-						</div>
-						<div className="w-full md:w-1/2 px-3">
-							<label htmlFor="description" className={inputStyle}>
-								Description
-							</label>
-							<Field
-								type="text"
-								id="description"
-								name="description"
-								placeholder="Product Description"
-								className={inputStyle1}
-							/>
-						</div>
-					</div>
+		<>
+			<h1 className="text-center text-2xl font-bold mt-8 mb-8">Register Product</h1>
 
-					<div className="flex flex-wrap -mx-3 mb-6">
-						<div className="w-full md:w-1/2 px-3">
-							<label htmlFor="price" className={inputStyle}>
-								Price
-							</label>
-							<Field type="text" id="price" name="price" placeholder="Product Price" className={inputStyle1} />
-							<p className="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
+			<Formik initialValues={InitialValues} onSubmit={handleSubmit}>
+				<div className="container mx-auto p-4">
+					<Form className="w-full max-w-lg">
+						<div className="flex flex-wrap -mx-3 mb-6">
+							<div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+								<label htmlFor="title" className={inputStyle}>
+									Title
+								</label>
+								<Field type="text" id="title" name="title" placeholder="Product Title" className={inputStyle1} />
+							</div>
+							<div className="w-full md:w-1/2 px-3">
+								<label htmlFor="description" className={inputStyle}>
+									Description
+								</label>
+								<Field
+									type="text"
+									id="description"
+									name="description"
+									placeholder="Product Description"
+									className={inputStyle1}
+								/>
+							</div>
 						</div>
 
-						<div className="w-full md:w-1/2 px-3">
-							<label htmlFor="discountPercentage" className={inputStyle}>
-								Discount Percentage
-							</label>
-							<Field
-								type="text"
-								id="discountPercentage"
-								name="discountPercentage"
-								placeholder="Discount Percentage"
-								className={inputStyle1}
-							/>
-						</div>
-					</div>
+						<div className="flex flex-wrap -mx-3 mb-6">
+							<div className="w-full md:w-1/2 px-3">
+								<label htmlFor="price" className={inputStyle}>
+									Price
+								</label>
+								<Field type="number" id="price" name="price" placeholder="Product Price" className={inputStyle1} />
+							</div>
 
-					<div className="flex flex-wrap -mx-3 mb-6">
-						<div className="w-full md:w-1/3 px-3">
-							<label htmlFor="stock" className={inputStyle}>
-								Stock
-							</label>
-							<Field type="text" id="stock" name="stock" placeholder="Product Stock" className={inputStyle1} />
-						</div>
-
-						<div className="w-full md:w-1/3 px-3">
-							<label htmlFor="category" className={inputStyle}>
-								Category
-							</label>
-							<Field type="text" id="category" name="category" placeholder="Product category" className={inputStyle1} />
+							<div className="w-full md:w-1/2 px-3">
+								<label htmlFor="discountPercentage" className={inputStyle}>
+									Discount Percentage
+								</label>
+								<Field
+									type="number"
+									id="discountPercentage"
+									name="discountPercentage"
+									placeholder="Discount Percentage"
+									className={inputStyle1}
+								/>
+							</div>
 						</div>
 
-						<div className="w-full md:w-1/3 px-3">
-							<label htmlFor="brand" className={inputStyle}>
-								Brand
-							</label>
-							<Field type="text" id="brand" name="brand" placeholder="Product Brand" className={inputStyle1} />
-						</div>
-					</div>
+						<div className="flex flex-wrap -mx-3 mb-6">
+							<div className="w-full md:w-1/3 px-3">
+								<label htmlFor="stock" className={inputStyle}>
+									Stock
+								</label>
+								<Field type="number" id="stock" name="stock" placeholder="Product Stock" className={inputStyle1} />
+							</div>
 
-					<div className="flex flex-wrap -mx-3 mb-6">
-						<div className="w-full md:w-1/3 px-3">
-							<label htmlFor="total_sell" className={inputStyle}>
-								Total Sell
-							</label>
-							<Field type="text" id="total_sell" name="total_sell" placeholder="Total Sell" className={inputStyle1} />
+							<div className="w-full md:w-1/3 px-3">
+								<label htmlFor="category" className={inputStyle}>
+									Category
+								</label>
+								<Field type="text" id="category" name="category" placeholder="Product category" className={inputStyle1} />
+							</div>
+
+							<div className="w-full md:w-1/3 px-3">
+								<label htmlFor="brand" className={inputStyle}>
+									Brand
+								</label>
+								<Field type="text" id="brand" name="brand" placeholder="Product Brand" className={inputStyle1} />
+							</div>
 						</div>
 
-						<div className="w-full md:w-1/3 px-3">
-							<label htmlFor="images" className={inputStyle}>
-								Images
-							</label>
-							<Field type="text" id="images" name="images" placeholder="Image URLs" className={inputStyle1} />
-						</div>
-					</div>
+						<div className="flex flex-wrap -mx-3 mb-6">
+							<div className="w-full md:w-1/3 px-3">
+								<label htmlFor="total_sell" className={inputStyle}>
+									Total Sell
+								</label>
+								<Field type="number" id="total_sell" name="total_sell" placeholder="Total Sell" className={inputStyle1} />
+							</div>
 
-					<div className="flex flex-wrap -mx-3 mb-6">
-						<div className="w-full px-3">
-							<button
-								type="submit"
-								className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-								Submit
-							</button>
+							<div className="w-full md:w-1/3 px-3">
+								<label htmlFor="images" className={inputStyle}>
+									Images
+								</label>
+								<Field type="text" id="images" name="images" placeholder="Image URLs" className={inputStyle1} />
+							</div>
 						</div>
-					</div>
-				</Form>
-			</div>
-		</Formik>
+
+						<div className="flex flex-wrap -mx-3 mb-6">
+							<div className="w-full px-3">
+								<button
+									type="submit"
+									className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+									Create Product
+								</button>
+							</div>
+						</div>
+					</Form>
+				</div>
+			</Formik>
+		</>
 	)
 }
 
