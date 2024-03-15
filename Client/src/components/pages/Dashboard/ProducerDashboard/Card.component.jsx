f7fccbbfd332ca8a9d4eb332fbc60b66f099e20b
VB5475 <vatsalb5463@outlook.com>
2024-03-15 09:46:41 +0530

    vatsal final push

--- Client/src/components/pages/Dashboard/ProducerDashboard/Card.component.jsx
+++ Client/src/components/pages/Dashboard/ProducerDashboard/Card.component.jsx
@@ -90,198 +90,154 @@ function Card({ card_data, hideButtons }) {
   return (
     <>
       {productdetails ? (
-        <div>
-          <div className="w-full max-w-xs bg-white border border-gray-200 rounded-lg shadow-lg ">
+        <div className="w-full md:max-w-xs lg:max-w-sm xl:max-w-md bg-white border border-gray-200 rounded-lg shadow-lg ">
+          <div>
+            <img
+              className="p-8 rounded-t-lg"
+              src={productdetails.thumbnail}
+              alt="product image"
+            />
+          </div>
+          <div className=" px-2 pb-2 md:px-5 md:pb-5">
             <div>
-              <img
-                className="p-8 rounded-t-lg"
-                src={productdetails.thumbnail}
-                alt="product image"
-              />
+              <h4 className="text-sm text-center font-semibold md:text-lg tracking-tight text-gray-900 mb-4">
+                {productdetails.title}
+              </h4>
             </div>
-            <div className="px-5 pb-5">
-              <div>
-                <h4 className="text-lg  font-semibold tracking-tight text-gray-900  mb-4">
-                  {productdetails.title}
-                </h4>
-              </div>
-              <div>
-                <h5 className="text-sm  font-normal tracking-tight text-gray-900  mb-4">
-                  {productdetails.description}
-                </h5>
-              </div>
-              <div className="flex items-center justify-between mb-4 ">
-                <span className="text-sm font-normal text-gray-900">
-                  <strong>Discount :</strong>{" "}
-                  {productdetails.discountPercentage}%
-                </span>
-                <span className="text-sm font-normal text-gray-900">
-                  <strong>Rating :</strong> {productdetails.rating}
-                </span>
-                <span className="text-sm font-normal text-gray-900">
-                  <strong>Quantity :</strong> {orderData.quantity}
-                </span>
-              </div>
+            <div className="md:text-left text-center">
+              <h5 className=" text-xs md:text-sm font-normal tracking-tight text-gray-900 mb-4">
+                {productdetails.description}
+              </h5>
+            </div>
 
-              {hideButtons ? (
-                editable ? (
-                  <div className="flex items-center justify-between text-black mb-4">
-                    <label className="text-black " htmlFor="delivery_calender">
-                      Delivery Date :
-                    </label>
-                    <input
-                      name="delivery_calender"
-                      id="delivery_calender"
-                      type="date" // Change type to text
-                      placeholder="select date to deliver"
-                      // value={orderData.expected_delivery} // Set value to fixed date
-                      // readOnly={!editable} // Set readOnly attribute to prevent editing
-                      autoFocus={editable}
-                      onChange={(e) => {
-                        const formattedDate = e.target.value;
-                        formattedDate.split("-").reverse().join("-");
-                        setSelectedDate(formattedDate);
-                        setDeliveryDateFilled(true);
-                      }}
-                      className={`bg-gray-200 px-3 py-1 w-1/2 rounded-md text-center ${
-                        editable ? "cursor-text" : "cursor-not-allowed"
-                      }`}
-                      required
-                    />
-                  </div>
-                ) : (
-                  <div className="flex items-center justify-between text-black mb-4">
-                    <label className="text-black " htmlFor="delivery_calender">
-                      Delivery Date :
-                    </label>
-                    <input
-                      name="delivery_calender"
-                      id="delivery_calender"
-                      type="text" // Change type to text
-                      placeholder="select date to deliver"
-                      value={orderData.expected_delivery
-                        .split("-")
-                        .reverse()
-                        .join("-")} // Set value to fixed date
-                      readOnly={!editable} // Set readOnly attribute to prevent editing
-                      autoFocus={editable}
-                      className={`bg-gray-200 px-3 py-1 w-1/2 rounded-md text-center ${
-                        editable ? "cursor-text" : "cursor-not-allowed"
-                      }`}
-                    />
-                  </div>
-                )
-              ) : (
-                <div className="flex items-center justify-between text-black mb-4 ">
-                  <label className="text-black " htmlFor="delivery_calender">
-                    delivery date :
+            <div className="flex flex-col md:flex-row  items-center justify-between mb-4 ">
+              <span className="text-xs md:text-sm font-normal text-gray-900">
+                <strong>Discount :</strong> {productdetails.discountPercentage}%
+              </span>
+              <span className="text-xs md:text-sm font-normal text-gray-900">
+                <strong>Rating :</strong> {productdetails.rating}
+              </span>
+              <span className="text-xs md:text-sm font-normal text-gray-900">
+                <strong>Quantity :</strong> {orderData.quantity}
+              </span>
+            </div>
+
+            {hideButtons ? (
+              editable ? (
+                <div className="flex flex-col items-center justify-between text-black mb-4">
+                  <label
+                    className="text-black text:xs md:text-base "
+                    htmlFor="delivery_calender"
+                  >
+                    Delivery Date :
                   </label>
                   <input
                     name="delivery_calender"
+                    id="delivery_calender"
                     type="date"
                     placeholder="select date to deliver"
-                    className="border-2 border-gray-200 rounded-md"
+                    autoFocus={editable}
                     onChange={(e) => {
                       const formattedDate = e.target.value;
                       formattedDate.split("-").reverse().join("-");
                       setSelectedDate(formattedDate);
                       setDeliveryDateFilled(true);
                     }}
+                    className={`bg-gray-200 px-3 py-1   rounded-md w-full text-center tracking-tight text:xs md:text-base ${
+                      editable ? "cursor-text" : "cursor-not-allowed"
+                    }`}
                     required
                   />
                 </div>
-              )}
-
-              {/* {editable && hide ? (
-                <>
-                  {hideButtons ? (
-                    <div className="flex items-center justify-between text-white mb-4">
-                      <label
-                        className="text-black "
-                        htmlFor="delivery_calender"
-                      >
-                        Delivery Date:
-                      </label>
-                      <input
-                        name="delivery_calender"
-                        id="delivery_calender"
-                        type="date" // Change type to text
-                        placeholder="select date to deliver"
-                        // Set value to fixed date
-                        // Set readOnly attribute to prevent editing
-
-                        className={`bg-gray-200 px-3 py-1 w-1/2 rounded-md ${
-                          editable ? "cursor-text" : "cursor-not-allowed"
-                        }`}
-                      />
-                    </div>
-                  ) : (
-                    <div className="flex items-center justify-between text-white mb-4 ">
-                      <label
-                        className="text-black "
-                        htmlFor="delivery_calender"
-                      >
-                        delivery date
-                      </label>
-                      <input
-                        name="delivery_calender"
-                        type="date"
-                        placeholder="select date to deliver"
-                        onChange={(e) => setSelectedDate(e.target.value)}
-                      />
-                    </div>
-                  )}
-                </>
               ) : (
-                <>
-                  
-                </>
-              )} */}
-
-              <div className="flex items-center justify-between ">
-                <span className="text-2xl font-bold text-gray-900 ">
-                  ${productdetails.price}
-                </span>
-
-                <div>
-                  {hideButtons ? (
-                    editable ? (
-                      <>
-                        <button
-                          onClick={handleSubmit}
-                          className="text-white bg-[#0295db] hover:bg-[#9d9da1] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 mx-1 text-center "
-                        >
-                          submit
-                        </button>
-                      </>
-                    ) : (
-                      <>
-                        <button
-                          onClick={handleDelay}
-                          className="text-white bg-[#0295db] hover:bg-[#9d9da1] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 mx-1 text-center "
-                        >
-                          Delay
-                        </button>
-                      </>
-                    )
-                  ) : (
-                    <>
-                      <button
-                        onClick={handleAccept}
-                        className="text-white bg-[#0295db] hover:bg-[#9d9da1] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 mx-1 text-center "
-                      >
-                        Accept
-                      </button>
-
-                      <button
-                        onClick={handleReject}
-                        className="text-white bg-[#0295db] hover:bg-[#9d9da1] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 text-center "
-                      >
-                        Reject
-                      </button>
-                    </>
-                  )}
+                <div className="flex flex-col items-center justify-between text-black mb-4">
+                  <label
+                    className="text-black text:xs md:text-base "
+                    htmlFor="delivery_calender"
+                  >
+                    Delivery Date :
+                  </label>
+                  <input
+                    name="delivery_calender"
+                    id="delivery_calender"
+                    type="text"
+                    placeholder="select date to deliver"
+                    value={orderData.expected_delivery
+                      .split("-")
+                      .reverse()
+                      .join("-")}
+                    readOnly={!editable}
+                    autoFocus={editable}
+                    className={`bg-gray-200 px-3 py-1   rounded-md w-full text-center tracking-tight text:xs md:text-base ${
+                      editable ? "cursor-text" : "cursor-not-allowed"
+                    }`}
+                  />
                 </div>
+              )
+            ) : (
+              //for pending orders
+              <div className="flex flex-col items-center justify-between text-black mb-4 ">
+                <label
+                  className="text-black text:xs md:text-base "
+                  htmlFor="delivery_calender"
+                >
+                  delivery date :
+                </label>
+                <input
+                  name="delivery_calender"
+                  type="date"
+                  placeholder="select date to deliver"
+                  className="border-2 border-gray-200 rounded-md w-full text-center tracking-tight text:xs md:text-base"
+                  onChange={(e) => {
+                    const formattedDate = e.target.value;
+                    formattedDate.split("-").reverse().join("-");
+                    setSelectedDate(formattedDate);
+                    setDeliveryDateFilled(true);
+                  }}
+                  required
+                />
+              </div>
+            )}
+
+            <div className="flex flex-row items-center justify-between ">
+              <span className="text-sm md:text-xl font-bold text-gray-900 ">
+                ${productdetails.price}
+              </span>
+
+              <div>
+                {hideButtons ? (
+                  editable ? (
+                    <button
+                      onClick={handleSubmit}
+                      className="text-white bg-[#0295db] hover:bg-[#9d9da1] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-1 mx-1 md:px-5 md:py-2  text-center "
+                    >
+                      submit
+                    </button>
+                  ) : (
+                    <button
+                      onClick={handleDelay}
+                      className="text-white bg-[#0295db] hover:bg-[#9d9da1] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-1 mx-1 md:px-5 md:py-2  text-center "
+                    >
+                      Delay
+                    </button>
+                  )
+                ) : (
+                  <div className="flex flex-row md:flex-row">
+                    <button
+                      onClick={handleAccept}
+                      className="text-white bg-[#0295db] hover:bg-[#9d9da1] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-1 mx-1 md:px-5 md:py-2  text-center "
+                    >
+                      Accept
+                    </button>
+
+                    <button
+                      onClick={handleReject}
+                      className="text-white bg-[#0295db] hover:bg-[#9d9da1] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-1 mx-1 md:px-5 md:py-2 text-center "
+                    >
+                      Reject
+                    </button>
+                  </div>
+                )}
               </div>
             </div>
           </div>
