051149717547ee89138007cbb0288d93b07e0af3
jay-n-prajapati <jay.n.prajapati@bacancy.com>
2024-03-19 16:07:29 +0530

    [REFACTOR] : code duplication removed from navbar

---
+++ Client/src/components/layout/Navbar/Links/Links.jsx
@@ -0,0 +1,37 @@
+
+import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+
+const CommonLinks = ({children , linksToRender}) => {
+  const { seller, admin } = useSelector((state) => state.role);
+
+ 
+
+  return (
+    <>
+      <ul className="font-medium text-base flex flex-col items-center gap-4 md:p-0 md:flex-row">
+
+        {linksToRender.map((element, idx) => {
+          return (
+            <li key={idx}>
+              <NavLink
+                to={element.slug}
+                className={({ isActive }) =>
+                  `${
+                    isActive ? "text-black" : "text-white"
+                  } flex items-center gap-2 text-lg py-1 hover:text-black`
+                }
+              >
+                {element.icon}
+                {element.label}
+              </NavLink>
+            </li>
+          );
+        })}
+        {children}
+      </ul>
+    </>
+  );
+};
+
+export default CommonLinks;
