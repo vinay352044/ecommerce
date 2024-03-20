b54fabd60b863dc7fb5f41d6e1dbcafb7d97fb9f
dipak-karmur <dipak.karmur@bacancy.com>
2024-03-20 14:27:17 +0530

    [(home): branch created]

--- Client/src/components/common/Card.jsx
+++ Client/src/components/common/Card.jsx
@@ -1,20 +1,97 @@
-import React from "react";
+// import React from "react";
+// import { Rating, RatingStar } from "flowbite-react";
+// import { CiHeart } from "react-icons/ci";
+// import { Link } from "react-router-dom";
+
+// function Card({ product, heartHandle, identifier, children }) {
+//   let flag;
+
+//   (identifier === "usersCard" || identifier === "wishlist") ? (flag = true): (flag = false);
+    
+
+
+//   return (
+//     <div>
+//       <div
+//         key={product.id}
+//         className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 "
+//       >
+//         {flag ? (
+//           <button className="justify-items-end h-5 w-6">
+//             <CiHeart onClick={() => heartHandle(product)} />
+//           </button>
+//         ) : null}
+
+//         <div className="overflow-hidden">
+//           <Link
+//             className={`${flag ? `` : `pointer-events-none`} `}
+//             to={`/products/${product.id}`}
+//           >
+//             <img
+//               className="p-8 rounded-t-lg"
+//               src={product.thumbnail}
+//               alt="product image"
+//             />
+//           </Link>
+//         </div>
+//         <div className="px-5 pb-5 ">
+//           <Link
+//             className={`${flag ? `` : `pointer-events-none`} `}
+//             to={`/products/${product.id}`}
+//           >
+//             <h5 className="text-xl font-semibold tracking-tight text-gray-900">
+//               {product.title}
+//             </h5>
+//             <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+//               {product.description}
+//             </p>
+//           </Link>
+//           <div className="flex flex-col md:flex-row  items-center justify-between mb-4 ">
+//             <span className="text-xs md:text-sm font-normal text-gray-900">
+//               <strong>Discount :</strong> {product.discountPercentage}%
+//             </span>
+//             <Rating>
+//               <RatingStar />
+//               <p className="ml-2 text-sm font-bold text-gray-900 dark:text-white">
+//                 {product.rating}
+//               </p>
+//             </Rating>
+//             {!flag ? (
+//               <span className="text-xs md:text-sm font-normal text-gray-900">
+//                 <strong>Quantity :</strong> {product.quantity}
+//               </span>
+//             ) : null}
+//           </div>
+//           {children}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Card;
+
+
+import React, { useState } from "react";
 import { Rating, RatingStar } from "flowbite-react";
 import { CiHeart } from "react-icons/ci";
 import { Link } from "react-router-dom";
 
 function Card({ product, heartHandle, identifier, children }) {
+  const [showFullDescription, setShowFullDescription] = useState(false);
   let flag;
 
-  identifier === "usersCard" || identifier === "wishlist"
-    ? (flag = true)
-    : (flag = false);
+  (identifier === "usersCard" || identifier === "wishlist") ? (flag = true) : (flag = false);
+
+  const toggleDescription = () => {
+    setShowFullDescription(!showFullDescription);
+  };
 
   return (
     <div>
       <div
         key={product.id}
-        className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 "
+        className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow h-"
       >
         {flag ? (
           <button className="justify-items-end h-5 w-6">
@@ -42,10 +119,20 @@ function Card({ product, heartHandle, identifier, children }) {
             <h5 className="text-xl font-semibold tracking-tight text-gray-900">
               {product.title}
             </h5>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            <p className={`mb-3 font-normal text-gray-700 dark:text-gray-400 ${showFullDescription ? '': 'truncate'}`}>
               {product.description}
             </p>
           </Link>
+          {!showFullDescription ? (
+              <button className="text-blue-600" onClick={toggleDescription}>
+                Read more
+              </button>
+            ):(
+              <button className="text-gray-400" onClick={toggleDescription}>
+                Read less
+              </button>
+            )
+          }
           <div className="flex flex-col md:flex-row  items-center justify-between mb-4 ">
             <span className="text-xs md:text-sm font-normal text-gray-900">
               <strong>Discount :</strong> {product.discountPercentage}%
