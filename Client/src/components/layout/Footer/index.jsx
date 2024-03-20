8378b60ccfb76bcb2afd14738efaf787c66acc26
meet-bhimani <meet.bhimani@bacancy.com>
2024-03-20 15:16:24 +0530

    [STYLE (footer)]:styling and responsiveness for footer is implemented

--- Client/src/components/layout/Footer/index.jsx
+++ Client/src/components/layout/Footer/index.jsx
@@ -2,23 +2,23 @@ import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <footer className="bg-[#0295db] rounded-lg shadow m-4">
-      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-3">
-        <div className="flex sm:items-center sm:justify-between">
-          <Link to="/" className="flex items-center mb-2 sm:mb-0 space-x-3 rtl:space-x-reverse">
+    <footer className="bg-[#0295db] shadow mt-10 py-5">
+      <div className="w-full mx-auto p-4 md:py-3">
+        <div className="text-center">
+          {/* <Link to="/" className="flex items-center mb-2 sm:mb-0 space-x-3 rtl:space-x-reverse">
             <img src="/images/svg/logo-black.svg" className="w-[10rem] h-[10rem] rounded-md" alt="Bac Mart Logo" />
-          </Link>
-          <ul className="w-full mx-auto flex flex-wrap items-center justify-end mb-2 text-sm font-medium text-white sm:mb-0  gap-4">
+          </Link> */}
+          <ul className="w-full mx-auto flex flex-wrap items-center justify-center mb-4 text-sm md:text-base lg:text-lg font-medium text-white sm:mb-0 gap-4">
             <li>
-              <Link to='/' className="hover:underline font-bold">Home</Link>
+              <Link to='/' className="hover:underline font-bold transition-all">Home</Link>
             </li>
             <li>
-              <Link to='/contact' className="hover:underline font-bold">Contact Us</Link>
+              <Link to='/contact' className="hover:underline font-bold transition-all">Contact Us</Link>
             </li>
           </ul>
         </div>
         <hr className="my-1 border-gray-200 sm:mx-auto  lg:my-1"  />
-        <span className="block  mx-auto text-sm text-white sm:text-center -mb-2 py-2">&copy; {new Date().getFullYear()} <a href="https://flowbite.com/" className="hover:underline">Bac Mart</a> | All Rights Reserved.</span>
+        <p className="w-full mx-auto text-sm text-white text-center mt-4 pt-2">&copy; {new Date().getFullYear()} <a href="https://flowbite.com/" className="hover:underline">Bac Mart</a> | All Rights Reserved.</p>
       </div>
     </footer>
   );
