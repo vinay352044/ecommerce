1d10d1320583de6a96afdde9c11dd3d7a7fe333c
meet-bhimani <meet.bhimani@bacancy.com>
2024-03-21 14:29:01 +0530

    [FIX(toast)]: changed position of toast notification

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -7,11 +7,11 @@ import "react-toastify/dist/ReactToastify.css"
 import Loader from "./components/common/Loader"
 
 const App = () => {
-	const router = Router();
+	const router = Router()
 	return (
 		<>
 			<Suspense fallback={<Loader />}>
-				<ToastContainer autoClose={2000} closeOnClick pauseOnFocusLoss={false} pauseOnHover transition={Bounce} />
+				<ToastContainer autoClose={2000} closeOnClick pauseOnFocusLoss={false} pauseOnHover transition={Bounce} style={{marginTop: '50px'}} />
 				<RouterProvider router={router} />
 			</Suspense>
 		</>
